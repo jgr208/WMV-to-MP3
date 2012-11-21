2ba99b4999c04a414fece72642f5212d3c17aa18
Jason Ricles <jasonricles@gmail.com>
2012-11-21 00:44:50 -0500

    Setup of classes

---
+++ FILES.java
@@ -0,0 +1,23 @@
+
+public class FILES {
+	public static void main(String[] args) 
+	{
+		welcome();
+		input();
+	}
+
+	private static void welcome() 
+	{
+		System.out.println("Welcome");
+	}
+
+	private static void input() 
+	{
+		System.out.println("WMV files would be put here");
+	}
+}
+
+class CONVERT
+{
+	
+}
